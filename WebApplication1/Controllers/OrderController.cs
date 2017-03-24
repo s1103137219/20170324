2a666774568719a26c0a34ec1a2faa3613501218
s1103137219 <sink0917@gmail.com>
2017-03-24 15:34:29 +0800

    132

---
+++ WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderService=new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+        public ActionResult Index2(string id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的Action(跑Model)
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            var result = new Models.Order() { CustId = "gss",CustName="瑞陽" };
+
+            return this.Json(result,JsonRequestBehavior.AllowGet);
+        }
+    }
+
+}
---
+++ WebApplication1/Models/OrderService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OrderService
+    {
+        
+       public void InsertOrder(Models.Order order)
+        {
+
+        }
+        public void DeleteOrderByID()
+        {
+
+        }
+        public void UpdateOrder()
+        {
+
+        }
+        public Models.Order GetOrderById(String id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "Gss";
+            result.CustName = "叡揚";
+            return result;
+        }
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+
+    }
+}
